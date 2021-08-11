e90928bb55ea70081a9010db10135158e9059ae7
Kenyi Rodriguez <kenyi.rodriguez@globant.com>
2021-08-11 12:23:47 -0500

    if let

--- General/ValoresOpcionales.playground/Contents.swift
+++ General/ValoresOpcionales.playground/Contents.swift
@@ -17,7 +17,6 @@ print(dato1 ?? -1)
 let resultado = dato2 + " / " + (dato3 ?? "")
 print(resultado)
 
-
 func sumarV1Numero1(_ n1: Int?, conNumero2 n2: Int?) -> Int {
     
     let suma = (n1 ?? 0) + (n2 ?? 0)
@@ -25,7 +24,6 @@ func sumarV1Numero1(_ n1: Int?, conNumero2 n2: Int?) -> Int {
 }
 
 sumarV1Numero1(nil, conNumero2: nil)
-
 
 func sumarV2Numero1(_ n1: Int?, conNumero2 n2: Int?) -> Int {
     
@@ -35,7 +33,6 @@ func sumarV2Numero1(_ n1: Int?, conNumero2 n2: Int?) -> Int {
 }
 
 sumarV2Numero1(nil, conNumero2: 10)
-
 
 //****************** guard let / guard var ********************
 /*
@@ -62,7 +59,9 @@ func sumar(n1: Int?, n2: Int?) {
     print("La suma es: \(suma)")
 }
 
+//Si pones un key a la estructura del tuple, entonces adoptan un llamado mediante el indice ordenado en el cual fueron agregados los datos.
 typealias UserRegister = (message: String, canAdded: Bool)
+typealias UserRegister2 = (message: [String], objAlumno: Alumno?)
 
 func registrarUsuario(nombre: String?, apellido: String?) -> UserRegister {
     
@@ -92,3 +91,60 @@ let datos: Persona = ("Rodrigo", "Salas", "9876543")
 datos.apellido
 datos.nombre
 datos.dni
+
+
+
+//****************** if let / guard var ********************
+/*
+    - Sólo funciona dentro de un método o función
+    - Sirve para crear variables seguras a partir de una insegura y realizar validaciones de flujo como si fuera un if
+    - En caso detecte un error en creación de la variable segura o condiciones, ésta ejecuta un flujo de error de modo opcional mediante el uso del else
+    - Las variables seguras que se crean, estan creadas dentro del contexto del if
+ */
+
+
+func sumarIf1(n1: Int?, n2: Int?) {
+    
+    if let n1 = n1 {
+        
+        if let n2 = n2 {
+            
+            let suma = n1 + n2
+            print("La suma es: \(suma)")
+            
+        } else {
+            print("Ingrese número 2")
+        }
+        
+    } else {
+        print("Ingrese número 1")
+    }
+}
+
+func sumarIf2(n1: Int?, n2: Int?) {
+    
+    if let n1Seguro = n1, let n2Seguro = n2 {
+        
+        let suma = n1Seguro + n2Seguro
+        print("La suma es: \(suma)")
+        
+    } else {
+        
+        print("Los datos ingresados son incorrectos")
+    }
+    
+    print("El proceso termino")
+}
+
+sumarIf2(n1: 4, n2: 10)
+
+
+/*
+ 
+ Single responsabilty
+ Open close
+ Liskov sustitution
+ Inteface segregation
+ Dependency inversion
+ 
+ */
